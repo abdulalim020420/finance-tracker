dcfee9608d6f5575cd6f73609a4ae4d0886e9c2b
abdulalim <abdulalim020420@gmail.com>
2026-07-16 00:36:16 +0800

    add vercel url domain at cors backend

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -66,7 +66,7 @@ export default function Register() {
                 value={form.name}
                 onChange={handleChange}
                 className="w-full border border-slate-200 rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-emerald-500"
-                placeholder="Abdul Alim"
+                placeholder="Enter Full Name"
                 required
               />
             </div>
